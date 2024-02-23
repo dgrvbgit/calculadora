067314d2820a396163cc4fd995e52b6c046e919d
dgrvbgit <dariogalvanrodriguezvb@gmail.com>
2024-02-23 18:08:45 +0100

    prueba_commit1

--- calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
+++ calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.calculadora;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -11,6 +13,35 @@ package com.mycompany.calculadora;
 public class Calculadora {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Programa Calculadora");
+        System.out.println("Ingrese el primer número:");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Ingrese el segundo número:");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Seleccione la operación a realizar:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        int opcion = sc.nextInt();
+        double resultado = 0;
+
+        switch (opcion) {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            default:
+                System.out.println("Opción no válida");
+                return; 
+        }
+
+        System.out.println("El resultado es: " + resultado);
     }
 }
+
+//prueba commit1
