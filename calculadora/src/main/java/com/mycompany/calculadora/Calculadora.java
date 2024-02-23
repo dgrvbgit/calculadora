722aca5de93b9e0d2a254c621b488d64d8ec798f
williamf-user <williamfernando.herreraaima@mprovidenciasa.com>
2024-02-23 19:07:09 +0100

    Añadiendo funcion

--- calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
+++ calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -8,9 +8,55 @@ package com.mycompany.calculadora;
  *
  * @author alumno
  */
-public class Calculadora {
+import java.util.Scanner;
 
+public class Calculadora {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Programa Calculadora");
+        System.out.println("Ingrese el primer número:");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Ingrese el segundo número:");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Seleccione la operación a realizar:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. División");
+        System.out.println("5. Potencia"); // Nueva opción
+
+        int opcion = sc.nextInt();
+        double resultado = 0;
+
+        switch (opcion) {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("No se puede dividir entre 0");
+                    return;
+                }
+                break;
+            case 5: // Nuevo caso para la potencia
+                resultado = Math.pow(num1, num2);
+                break;
+            default:
+                System.out.println("Opción no válida");
+                return;
+        }
+
+        System.out.println("El resultado es: " + resultado);
     }
 }
